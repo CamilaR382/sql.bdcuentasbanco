e6af4e28449626a9171dcf38d98e2ae1b36a5467
CamilaR382 <camila_mai1238@hotmail.com.ar>
2024-08-16 19:50:21 -0300

    se agrega act

--- desfin.py
+++ desfin.py
@@ -1,27 +1,43 @@
-''
 import json
+
 class CuentaBancaria :
    def __init__(self, titular_de_la_cuenta, numero_de_cuenta, dni, monto) :
-     self.titular_de_la_cuenta = titular_de_la_cuenta
-     self.numero_de_cuenta = numero_de_cuenta
-     self.dni = self.validar_dni=(dni)
-     self.monto = self.validar_monto=(monto)
+       self.titular_de_la_cuenta = titular_de_la_cuenta
+       self.numero_de_cuenta = numero_de_cuenta
+       self.dni = self.validar_dni=(dni)
+       self.monto = self.validar_monto=(monto)
 
-     @property
-     def titular_de_la_cuenta(self):
-      return self.titular_de_la_cuentacapitalize()
-     @property
-     def numero_de_cuenta(self):
+   @property
+   def titular_de_la_cuenta(self):
+       return self.titular_de_la_cuenta
+   
+   @titular_de_la_cuenta.setter
+   def titular_de_la_cuenta(self, value):
+        self.titular_de_la_cuenta = value.capitalize()
+
+   @property
+   def numero_de_cuenta(self):
         return self.numero_de_cuenta()
-     @property
-     def dni(self):
+     
+   @numero_de_cuenta.setter
+   def numero_de_cuenta(self, value):
+        self.numero_de_cuenta = value
+
+   @property
+   def dni(self):
        return self.dni()
-     @property
-     def monto(self):
+     
+   @dni.setter
+   def dni(self, value):
+        self._dni = self.validar_dni(value)
+     
+   @property
+   def monto(self):
         return self.monto()
-     @monto.setter
-     def monto(self, nuevo_monto):
-      self.monto = self.validar_monto(nuevo_monto)
+     
+   @monto.setter
+   def monto(self, value):
+      self._monto = self.validar_monto(value)
 
    def validar_dni(self, dni):
         try:
@@ -34,11 +50,11 @@ class CuentaBancaria :
         except ValueError as e:
             raise ValueError(f"El DNI es invalido: {e}")
 
-   def validar_saldo(self, monto):
+   def validar_monto(self, monto):
         try:
             monto_num = float(monto)
             if monto_num < 0: 
-               raise ValueError("El saldo debe ser positivo.")
+                raise ValueError("El saldo debe ser positivo.")
             return monto_num
         except ValueError as e:
             raise ValueError(f"Saldo inválido: {e}") 
@@ -52,67 +68,41 @@ class CuentaBancaria :
      }
 
    def __str__(self):
-    return f"Cuenta Bancaria: { self.numero_de_cuenta }, Titular: {self.titular_de_la_cuenta}, DNI: {self.dni}, Saldo: {self.monto}"
+       return f"Cuenta Bancaria: { self.numero_de_cuenta }, Titular: {self.titular_de_la_cuenta}, DNI: {self.dni}, Saldo: {self.monto}"
 
-class CuentaBancariaCorriente (CuentaBancaria) :
+class CuentaBancariaCorriente (CuentaBancaria):
      def __init__(self, titular_de_la_cuenta,  numero_de_cuenta, dni, monto):
-      super().__init__(titular_de_la_cuenta, numero_de_cuenta, dni, monto) 
-
-      self.titular_de_la_cuenta = titular_de_la_cuenta
-
-     @property
-     def numero_de_cuenta(self):
-      return self.numero_de_cuenta
+         super().__init__(titular_de_la_cuenta, numero_de_cuenta, dni, monto) 
      
      def depositar(self, monto):
         if monto < 0:
            print("ERROR: monto no puede ser negativo")
         else:
          self.monto += monto
-        print ("Se depositó {monto} en la cuenta {self.numero_de_cuenta}. Nuevo saldo es {self.monto}")
+         print ("Se depositó {monto} en la cuenta {self.numero_de_cuenta}. Nuevo saldo es {self.monto}")
 
-
-     def retirar(self, monto) :
-        
+     def retirar(self, monto):
        if monto < 0:
            print("ERROR: el monto no puede ser negativo")
        elif monto > self.monto:  
-        print("ERROR: fondos insuficientes")
+           print("ERROR: fondos insuficientes")
        else:
-            self.monto += monto
-       print("Se retiró {monto} en la cuenta {self.numero_de_cuenta}. Nuevo saldo es {self.monto}")
-
+            self.monto -= monto
+            print("Se retiró {monto} en la cuenta {self.numero_de_cuenta}. Nuevo saldo es {self.monto}")
 
      def transferir(self, monto, otra_cuenta):
        if monto < 0:
            print("ERROR: el monto no puede ser negativo")
        elif monto > self.monto:
-        print("ERROR: fondos insuficientes")
-       if otra_cuenta not in CuentaBancaria:
-        print("ERROR: Cuenta destino no existe.")
+            print("ERROR: fondos insuficientes")
        else:
         self.monto -= monto
-       CuentaBancaria[otra_cuenta].monto += monto
-       print("Se transfirió {monto} en la cuenta {otra_cuenta}. Nuevo saldo es {self.monto}")
-
-
+       otra_cuenta.monto += monto
+       print(f"Se transfirió {monto} en la cuenta {otra_cuenta.numero_de_cuenta}. Nuevo saldo es {self.monto}")
    
 class CuentaBancariaAhorro (CuentaBancaria) :
      def __init__(self, titular_de_la_cuenta,  numero_de_cuenta, dni, monto):
-      super().__init__(numero_de_cuenta, dni, monto) 
-      self.titular_de_la_cuenta : titular_de_la_cuenta
-
-     @property
-     def numero_de_cuenta(self):
-      return self.numero_de_cuenta
-     
-     def to_dict(self):
-        data = super().to_dict()
-        data["CuentaBancaria"] = self.monto
-        return data
-
-     def __str__(self):
-        return f"{super().__str__()} - Cuenta Bancaria: {self.monto}"
+      super().__init__(self, titular_de_la_cuenta, numero_de_cuenta, dni, monto) 
 
 
      def depositar(self, monto):
@@ -120,78 +110,72 @@ class CuentaBancariaAhorro (CuentaBancaria) :
            print("ERROR: monto no puede ser negativo")
         else:
          self.monto += monto
-        print ("Se depositó {monto} en la cuenta {self.numero_de_cuenta}. Nuevo saldo es {self.monto}")
+         print ("Se depositó {monto} en la cuenta {self.numero_de_cuenta}. Nuevo saldo es {self.monto}")
 
 
      def retirar(self, monto) :
-        
        if monto < 0:
            print("ERROR: el saldo no puede ser negativo")
        elif monto > self.monto:  
-        print("ERROR: fondos insuficientes")
+            print("ERROR: fondos insuficientes")
        else:
-            self.monto += monto
-       print("Se retiró {monto} en la cuenta {self.numero_de_cuenta}. Nuevo saldo es {self.monto}")
+        self.monto += monto
+        print("Se retiró {monto} en la cuenta {self.numero_de_cuenta}. Nuevo saldo es {self.monto}")
 
 
      def transferir(self, monto, otra_cuenta):
        if monto < 0:
            print("ERROR: el saldo no puede ser negativo")
        elif monto > self.monto:
-        print("ERROR: fondos insuficientes")
-       if otra_cuenta not in CuentaBancaria:
-        print("ERROR: Cuenta destino no existe.")
+             print("ERROR: fondos insuficientes")
        else:
-        self.monto -= monto
-       CuentaBancaria[otra_cuenta].monto += monto
-       print("Se transfirió {monto} en la cuenta {otra_cuenta}. Nuevo saldo es {self.monto}")
+           self.monto -= monto
+           otra_cuenta.monto += monto
+           print("Se transfirió {monto} en la cuenta {otra_cuenta.numero_de_cuenta}. Nuevo saldo es {self.monto}")
 
 class CuentasdeBancoCH:
  def __init__(self, archivo):
         self.archivo = archivo
+        self.datos = self.leer_datos()
 
-def leer_datos(self):
+ def leer_datos(self):
         try:
             with open(self.archivo, 'r') as file:
-                titular_de_la_cuenta = json.load(file)
+                return json.load(file)
         except FileNotFoundError:
             return {}
         except Exception as error:
             raise Exception(f'Error al leer datos del archivo: {error}')
-        else:
-            return titular_de_la_cuenta
 
-def guardar_datos(self, titular_de_la_cuenta):
-        try:
+ def guardar_datos(self):
+         try:
             with open(self.archivo, 'w') as file:
-                json.dump(titular_de_la_cuenta, file, indent=4)
-        except IOError as error:
+                json.dump(self.datos, file, indent=4)
+         except IOError as error:
             print(f'Error al intentar guardar los datos en {self.archivo}: {error}')
-        except Exception as error:
+         except Exception as error:
             print(f'Error inesperado: {error}')
 
-def crear_cliente(self, numero_de_cuenta, cliente):
-        try:
-            numero_de_cuenta = self.leer_datos()
+ def crear_cliente(self, cliente):
+         try:
             dni = cliente.dni
-            if not str(dni) in numero_de_cuenta.keys():
-                cliente[dni] = numero_de_cuenta.to_dict()
-                self.guardar_numero_de_cuenta(numero_de_cuenta)
+            if not str(dni) not in self.datos:
+                self.datos[dni] = cliente.to_dict()
+                self.guardar_datos()
                 print(f'Guardado exitoso')
             else:
-                print(f'Colaborador con DNI {dni} ya existe')
-        except Exception as error:
-            print(f'Error inesperado al crear colaborador: {error}')
+                print(f'Cliente con DNI {dni} ya existe')
+         except Exception as error:
+               print(f'Error inesperado al crear cliente: {error}')
 
-def eliminar_datos_titular(self, titular_de_la_cuenta, dni):
-      try:
-       datos = self.leer_datos()
-       if str(titular_de_la_cuenta) in datos.keys():
-           datos[titular_de_la_cuenta]
-           self.guardar_datos(datos)
-           print(f'Titular de la Cuenta {titular_de_la_cuenta} eliminado correctamente')
-       else:
-        print(f'No se encontró titular de la cuenta con DNI: {dni}')
-      except Exception as e:
-       print(f'Error al eliminar Titular:{e}')
+ def buscar_cliente(self, dni):
+         return self.datos.get(dni, "Cliente no encontrado")  
+ '''''        
 
+ def eliminar_datos_titular(self, dni):
+         if dni in self.datos:
+          del self.guardar_datos()
+          print(f'Titular de la Cuenta {dni} eliminado correctamente.')
+         else:
+          print(f'No se encontró titular de la cuenta con DNI: {dni}')
+ '''''
--- main manual.py
+++ main manual.py
@@ -1,95 +1,38 @@
-from desfin import (CuentaBancaria, CuentaBancariaCorriente, CuentaBancariaAhorro, CuentasdeBancoCH)
-sistema = CuentaBancaria('cuentas_bancarias.json')
+import os 
+import platform 
+from desfin import (CuentaBancaria, CuentaBancariaCorriente, CuentaBancariaAhorro)
 
-sistema.mostrar_menu(CuentaBancaria, CuentaBancariaCorriente, CuentaBancariaAhorro, CuentasdeBancoCH)
-def mostrar_menu():
- print('Menu CuentaBancaria')
-print('1. Cuenta Bancaria')
-print('2. Cuenta Corriente')
-print('3. Cuenta de Ahorro')
-print('4. Salir')
-print('---------------------------------')
+def limpiar_pantalla():
+    ''' Limpiar la Pantalla segun el sistema operativo '''
+    if platform.system() == 'windows':
+        os.system('cls')
+    else:
+        os.system('clear')
 
-
-cuenta_corriente = CuentaBancaria()
-sistema.crear_cliente()
+sistema = CuentaBancaria ('C:\Users\Usuario\Documents\Analisis de Datos ETAPA 3 2024\nueva carpeta\datos.json')
 print("Crear nueva Cuenta Bancaria:")
 titular_de_la_cuenta = input("Ingrese el nombre del titular: ")
 numero_de_cuenta = input("Ingrese el número de cuenta: ")
-dni = input("Ingrese el DNI: ")
-saldo = float(input("Ingrese el saldo inicial: "))
+dni = int(input("Ingrese el DNI: "))
+monto = float(input("Ingrese el saldo inicial: "))
 
-cuenta_corriente = CuentaBancariaCorriente()
-sistema.crear_cliente()
-print("Crear nueva Cuenta Bancaria:")
+cuenta_corriente = CuentaBancariaCorriente(titular_de_la_cuenta, numero_de_cuenta, dni, monto)
+
+sistema.crear_cliente(cuenta_corriente)
+
+print("Crear nueva Cuenta de Ahorro:")
 titular_de_la_cuenta = input("Ingrese el nombre del titular: ")
 numero_de_cuenta = input("Ingrese el número de cuenta: ")
 dni = input("Ingrese el DNI: ")
-saldo = float(input("Ingrese el saldo inicial: "))
+monto = float(input("Ingrese el saldo inicial: "))
 
-'''
-def __init__(self, archivo):
-  self.archivo = archivo
-
-def leer_datos(self):
-   try:
-     with open(self.archivo, 'r') as file:
-       datos = (file)
-   except Exception as error:
-       raise Exception(f'Error al leer datos del archivo: {error}')  
-   else:
-      return datos
-   
-def actualizar_datos_titular(self,numero_de_cuenta, monto):
- try:
-     datos = self.leer_datos()
-     if str(monto) in datos.keys():
-       datos[numero_de_cuenta]['monto'] = monto
-       self.guardar_datos(datos)
-       print(f'Monto actualizado para el número de cuenta: {numero_de_cuenta}')
-     else:
-       print(f'No se encontró titular con numero de cuenta: {numero_de_cuenta}')
- except Exception as e:
-   print(f'Error al actualizar el monto: {e}')
-
-def eliminar_datos_titular(self, titular_de_la_cuenta):
-  try:
-   datos = self.leer_datos()
-   if str(titular_de_la_cuenta) in datos.keys():
-      datos[titular_de_la_cuenta]
-      self.guardar_datos(datos)
-      print(f'Titular de la Cuenta {titular_de_la_cuenta} eliminado correctamente')
-   else:
-      print(f'No se encontró titular de la cuenta con DNI: {dni}')
-  except Exception as e:
-   print(f'Error al eliminar Titular:{e}')
-'''
-
-cuenta_corriente = CuentaBancariaCorriente(titular_de_la_cuenta, numero_de_cuenta, dni, saldo)
-sistema.crear_cliente(titular_de_la_cuenta)
-
-print("Crear nueva Cuenta Corriente:")
-titular_de_la_cuenta = input("Ingrese el nombre del titular: ")
-numero_de_cuenta = input("Ingrese el número de cuenta: ")
-dni = input("Ingrese el DNI: ")
-saldo = float(input("Ingrese el saldo inicial: "))
-
-cuenta_corriente = CuentaBancariaCorriente(titular_de_la_cuenta, numero_de_cuenta, dni, saldo)
-sistema.crear_cliente(titular_de_la_cuenta)
-
-print("\nCrear nueva Cuenta de Ahorro:")
-titular_de_la_cuenta = input("Ingrese el nombre del titular: ")
-numero_de_cuenta = input("Ingrese el número de cuenta: ")
-dni = input("Ingrese el DNI: ")
-saldo = float(input("Ingrese el saldo inicial: "))
-
-cuenta_ahorro = CuentaBancariaAhorro(titular_de_la_cuenta, numero_de_cuenta, dni, saldo)
+cuenta_ahorro = CuentaBancariaAhorro(titular_de_la_cuenta, numero_de_cuenta, dni, monto)
 sistema.crear_cliente(cuenta_ahorro)
 
 deposito = float(input("\nIngrese el monto a depositar en la Cuenta Corriente: "))
 cuenta_corriente.depositar(deposito)
 
-retiro = float(input("\nIngrese el monto a retirar de la Cuenta de Ahorro: "))
+retiro = float(input("\nIngrese el monto a retirar de la Cuenta : "))
 cuenta_ahorro.retirar(retiro)
 
 transferencia = float(input("\nIngrese el monto a transferir de la Cuenta de Ahorro a la Cuenta Corriente: "))
@@ -103,4 +46,3 @@ sistema.guardar_datos()
 
 dni_buscar = input("\nIngrese el DNI para buscar la cuenta en el sistema: ")
 print(sistema.buscar_cliente(dni_buscar))
-
